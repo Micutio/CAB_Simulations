d5a68cc5143825bf7c6c63dd35a6cafd914f34c2
mWagner <michael.wagner@tum-create.edu.sg>
2017-03-21 16:14:25 +0800

    Added: example simulation script for testing cellular automaton properties

---
+++ SimTemplateSingleFile/hex_tests_demo.py
@@ -0,0 +1,166 @@
+"""
+Main module of the Flow and Pressure Demo.
+Uses the Complex Automaton Base.
+"""
+
+
+# External library imports.
+import pygame
+import numpy
+
+# CAB system imports.
+from cab.ca.cab_ca_hex import CAHex
+from cab.ca.cab_cell import CellHex
+from cab.cab_global_constants import GlobalConstants
+from cab.cab_system import ComplexAutomaton
+from cab.util.cab_input_handling import InputHandler
+from cab.util.cab_visualization import Visualization
+
+
+__author__ = 'Michael Wagner'
+__version__ = '1.0'
+
+
+class GC(GlobalConstants):
+    def __init__(self):
+        super().__init__()
+        self.VERSION = 'version: 09-2014'
+        ################################
+        #     SIMULATION CONSTANTS     #
+        ################################
+        self.RUN_SIMULATION = False
+        self.ONE_AGENT_PER_CELL = False
+        ################################
+        #         CA CONSTANTS         #
+        ################################
+        self.USE_HEX_CA = True
+        self.USE_CA_BORDERS = False
+        self.DIM_X = 100  # How many cells is the ca wide?
+        self.DIM_Y = 100  # How many cells is the ca high?
+        self.CELL_SIZE = 7  # How long/wide is one cell?
+        self.GRID_WIDTH = self.DIM_X * self.CELL_SIZE
+        self.GRID_HEIGHT = self.DIM_Y * self.CELL_SIZE
+        ################################
+        #        ABM CONSTANTS         #
+        ################################
+        ################################
+        #      UTILITY CONSTANTS       #
+        ################################
+
+
+class SimpleCell(CellHex):
+    def __init__(self, x, y, gc):
+        super().__init__(x, y, gc)
+        self.state = False
+
+    def sense_neighborhood(self):
+        # for cell in self.neighbors:
+        pass
+
+    def update(self):
+        pass
+
+    def clone(self, x, y):
+        return SimpleCell(x, y, self.gc)
+
+
+class SimpleIO(InputHandler):
+    def __init__(self, cab_sys):
+        super().__init__(cab_sys)
+        self.dist = 1
+        self.show_direction_state = 0
+        self.cell_a = None
+        self.cell_b = None
+        self.cell_c = None
+
+    def clone(self, cab_sys):
+        return SimpleIO(cab_sys)
+
+    def custom_mouse_action(self, button):
+        # Click on left mouse button.
+        if button == 1:
+            # self.show_neighbors_up_to_dist()
+            # self.show_cell_in_direction()
+            self.show_neighbors()
+        # Click on middle mouse button / mouse wheel
+        elif button == 2:
+            pass
+
+        # Click on right mouse button
+        elif button == 3:
+            pass
+
+    def show_cell_in_direction(self):
+        if self.show_direction_state == 0:
+            a_x, a_y = self.get_mouse_hex_coords()
+            self.cell_a = self.sys.ca.ca_grid[a_x, a_y]
+            self.cell_a.state = True
+            self.show_direction_state = 1
+        elif self.show_direction_state == 1:
+            b_x, b_y = self.get_mouse_hex_coords()
+            self.cell_b = self.sys.ca.ca_grid[b_x, b_y]
+            self.cell_b.state = True
+            self.show_direction_state = 2
+            c_x, c_y = CAHex.get_cell_in_direction(self.cell_a, self.cell_b)
+            self.cell_c = self.sys.ca.ca_grid[c_x, c_y]
+            self.cell_c.state = True
+            cube_dist = CAHex.cube_distance(self.cell_a, self.cell_b)
+            hex_dist = CAHex.hex_distance(self.cell_a.q, self.cell_a.r, self.cell_b.q, self.cell_b.r)
+            print("cube distance between a and b: {0}".format(cube_dist))
+            print("hex distance between a and b: {0}".format(hex_dist))
+        elif self.show_direction_state == 2:
+            self.cell_a.state = False
+            self.cell_b.state = False
+            self.cell_c.state = False
+            self.show_direction_state = 0
+        else:
+            print('We should never enter this branch!')
+
+    def show_neighbors_up_to_dist(self):
+        cell_x, cell_y = self.get_mouse_hex_coords()
+        neighborhood = self.sys.ca.get_cell_neighborhood(cell_x, cell_y, self.dist)
+        for v in list(self.sys.ca.ca_grid.values()):
+            v.state = False
+        for v in list(neighborhood.values()):
+            v.state = True
+
+    def show_neighbors(self):
+        cell_x, cell_y = self.get_mouse_hex_coords()
+        print('selected cell: {0}, {1}'.format(cell_x, cell_y))
+        for n in self.sys.ca.ca_grid[cell_x, cell_y].neighbors:
+            n.state = True
+
+
+class SimpleVis(Visualization):
+    def __init__(self, global_const, sys,  screen):
+        super().__init__(global_const, sys, screen)
+
+    def clone(self, cab_sys, screen):
+        return SimpleVis(self.gc, cab_sys, screen)
+
+    def draw_cell(self, cell):
+        """
+        Simple exemplary visualization. Draw cell in white.
+        """
+        if cell is None:
+            pass
+        else:
+            if cell.state is True:
+                pygame.gfxdraw.filled_polygon(self.surface, cell.get_corners(), (200, 0, 55))
+            elif cell.state is False and not cell.is_border:
+                pygame.gfxdraw.filled_polygon(self.surface, cell.get_corners(), (69, 79, 89))
+            elif cell.state is False and cell.is_border:
+                pygame.gfxdraw.filled_polygon(self.surface, cell.get_corners(), (89, 79, 69))
+            else:
+                pygame.gfxdraw.filled_polygon(self.surface, cell.get_corners(), (0, 255, 0))
+        pygame.gfxdraw.aapolygon(self.surface, cell.get_corners(), (0, 0, 0))
+
+
+if __name__ == '__main__':
+    gc = GC()
+    pc = SimpleCell(0, 0, gc)
+    ph = SimpleIO(None)
+    pv = SimpleVis(gc, None, None)
+    simulation = ComplexAutomaton(gc, proto_cell=pc, proto_handler=ph, proto_visualizer=pv)
+    simulation.run_main_loop()
+    # cProfile.run("simulation.run_main_loop()")
