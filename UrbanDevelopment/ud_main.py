83a9bed4e5a7cb7fd62bca24cc9a7b8b91b13262
mWagner <michael.wagner@tum-create.edu.sg>
2017-03-31 17:11:14 +0800

    Added: description of model idea to main class

--- UrbanDevelopment/ud_main.py
+++ UrbanDevelopment/ud_main.py
@@ -1,5 +1,26 @@
 """
 Main module of the urban development simulation.
+This shall be used to experiment with growth models of cities or urban areas in
+general.
+
+Idea of v1.0:
+    Three different types of land use (residential, commercial, industrial).
+    Residental cells grow/are created when there is a lot of commerce.
+    Commercial cells grow/are created when there is a lot of industry.
+    Industrial cells grow/are created when there is a lot of residents.
+    Residental cells are negatively impacted by nearby industrial cells.
+
+    Look into the following entities and how they can be incorporated:
+    - roads
+    |--> making distant cells available
+    - natural resources
+    |--> create incentives for settlements
+    |--> create boost for industry
+    - terrain
+    |--> promote/inhibit spreading rate of cells
+    - agents
+    |--> flow of goods and people
+
 """
 
 from cab.cab_system import ComplexAutomaton
