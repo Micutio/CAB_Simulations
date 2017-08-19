aee126c9a8788e45e8e84a0ba1eefcefe20e3bb8
micutio <wagner.mchl@googlemail.com>
2017-08-20 00:06:12 +0800

    Added: first implementation of urban development scenario: primitive growth

--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # CAB_Simulations
-This repository contains a collection of cellular automata / agent-based simulations using the ComplexAutomataBase (CAB) framework.
+This repository contains a collection of cellular automata / agent-based simulations using the
+ComplexAutomataBase (CAB) framework.
 
 
 ## Foraging Ant
 
 ### Description
 
-Foraging Ant describes the teamwork of ants on their mission to find food and deliver it back to their anthill. By using two sorts of pheromones to mark their trails back and forth, these tiny soldiers manage to implicity communicate with one another and eventually create a nearly optimized trail.
+Foraging Ant describes the teamwork of ants on their mission to find food and deliver it back to
+their anthill. By using two sorts of pheromones to mark their trails back and forth, these tiny
+soldiers manage to implicitly communicate with one another and eventually create a nearly optimized
+trail.
 
 Simple features as
 
@@ -23,8 +27,34 @@ lead to an organic seeming behavior and lively little anthills.
 
 ## Sugarscape [Alpha Version, Under Construction]
 
-The Sugarscape is a world of sugar and spice. Agents will roam the land and harvest the resources to stay alive. Agents can mate and create offspring that will carry on their genes. Simple rules allow for complex behavior, leading to migrations, diseases, population fluctuations, trading and more.
+The Sugarscape is a world of sugar and spice. Agents will roam the land and harvest the resources
+to stay alive. Agents can mate and create offspring that will carry on their genes. Simple rules
+allow for complex behavior, leading to migrations, diseases, population fluctuations, trading and
+more.
 
-## Urban Development [Planned]
+## Urban Development [Under Construction]
 
-Urban Development will explore organic growth of cities or other urban ecologies by using the RICO model, whereby urban landuse is divided into three principal designations: residential, industrial and commercial. The ultimate goal will be to find non-complex rules that lead an intial seed, of one or more of these landuses, to grow into a plausible city shape over time.
+Urban Development will explore organic growth of cities or other urban ecologies by using the RICO
+model, whereby urban land use is divided into three principal designations: residential, industrial
+and commercial. The ultimate goal will be to find non-complex rules that lead an initial seed, of
+one or more of these land uses, to grow into a plausible city shape over time.
+
+### Version 1 - Primitive Growth
+
+With the first version we will try to get a feeling for underlying rules of urban development.
+These could be the following:
+
+* residential zones want commerce to acquire goods and services to live
+* commercial zones want industry to acquire goods to sell
+* industrial zones want residents to populate their factories
+
+It is apparent that the relationships between all three zones form a cycle. How can we derive rules
+for zone growth from these observations? If we look at [Conway's Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life),
+it divides growth into three categories. A cell can come to life, stay alive or die, depending on
+the number of living and dead neighbors. If we transfer this approach to our zones, it might look
+like this:
+
+* if an empty zone has a neighbor with a commercial zone, then it can become a residential zone
+* if an empty zone has a neighbor with an industrial zone, then it can become a commercial zone
+* if an empty zone has a neighbor with a residential zone, then it can become an industrial zone
+* if two or more criteria are met, the empty zone chooses its new designation at random 
--- UrbanDevelopment/ca/ud_cell.py
+++ UrbanDevelopment/ca/ud_cell.py
@@ -4,6 +4,8 @@ Module containing the cell definition for the urban world.
 
 from cab.ca.cab_cell import CellHex
 
+from cab.util.cab_rng import get_RNG
+
 __author__ = 'Michael Wagner'
 __version__ = '1.0'
 
@@ -16,30 +18,67 @@ class WorldCell(CellHex):
     def __init__(self, x, y, gc):
         super().__init__(x, y, gc)
         self.zone = EmptyZone()
+        self.neighbor_zones = set()
 
     def clone(self, x, y):
         return WorldCell(x, y, self.gc)
 
     def sense_neighborhood(self):
-        self.num_neighbors = 0
-        for n in self.neighbors:
-            self.num_neighbors += 1
+        if self.zone.type == 'EMPTY':
+            for n in self.neighbors:
+                if n.zone.type == "RESIDENTIAL":
+                    self.neighbor_zones.add("INDUSTRIAL")
+                elif n.zone.type == "COMMERCIAL":
+                    self.neighbor_zones.add("RESIDENTIAL")
+                elif n.zone.type == "INDUSTRIAL":
+                    self.neighbor_zones.add("COMMERCIAL")
+        else:
+            self.zone.sense_neighborhood(self)
 
     def update(self):
         """
         This method regulates the cell's temperature according to the temperature of its neighbors.
         """
-        pass
+        if self.zone == 'EMPTY':
+            self.zone.update(self)
+        else:
+            if len(self.neighbor_zones) > 0:
+                new_zone = get_RNG().choice(list(self.neighbor_zones))
+                if new_zone == 'RESIDENTIAL':
+                    self.zone = ResidentialZone()
+                elif new_zone == 'COMMERCIAL':
+                    self.zone = CommercialZone()
+                elif new_zone == 'INDUSTRIAL':
+                    self.zone = IndustrialZone()
+                self.color = self.zone.color
+        self.neighbor_zones.clear()
+
+    def on_lmb_click(self, abm, ca):
+        """
+        On left mouse click, cycle through zones.
+        """
+        if self.zone.type == "EMPTY":
+            self.zone = ResidentialZone()
+        elif self.zone.type == 'RESIDENTIAL':
+            self.zone = CommercialZone()
+        elif self.zone.type == 'COMMERCIAL':
+            self.zone = IndustrialZone()
+        elif self.zone.type == 'INDUSTRIAL':
+            self.zone = EmptyZone()
+        self.color = self.zone.color
 
 class EmptyZone():
     """
-    This class residential zones.
+    This class represents empty zones.
     """
     def __init__(self):
         self.type = "EMPTY"
         self.color = (0, 0, 0)
 
-    def update(self):
+    def sense_neighborhood(self, parent_cell):
+        pass
+
+    def update(self, parent_cell):
         """
         This method regulates the cell's temperature according to the temperature of its neighbors.
         """
@@ -48,13 +87,16 @@ class EmptyZone():
 
 class ResidentialZone():
     """
-    This class residential zones.
+    This class represents residential zones.
     """
     def __init__(self):
         self.type = "RESIDENTIAL"
         self.color = (0, 255 , 0)
 
-    def update(self):
+    def sense_neighborhood(self, parent_cell):
+        pass
+
+    def update(self, parent_cell):
         """
         This method regulates the cell's temperature according to the temperature of its neighbors.
         """
@@ -63,13 +105,16 @@ class ResidentialZone():
 
 class CommercialZone():
     """
-    This class commercial zones.
+    This class represents commercial zones.
     """
     def __init__(self):
         self.type = "COMMERCIAL"
         self.color = (0, 0, 255)
 
-    def update(self):
+    def sense_neighborhood(self, parent_cell):
+        pass
+
+    def update(self, parent_cell):
         """
         This method regulates the cell's temperature according to the temperature of its neighbors.
         """
@@ -78,13 +123,16 @@ class CommercialZone():
 
 class IndustrialZone():
     """
-    This class industrial zones.
+    This class represents industrial zones.
     """
-    def __init__(self, x, y, gc):
+    def __init__(self):
         self.type = "INDUSTRIAL"
-        self.color = (0, 255 , 255)
+        self.color = (255, 255 , 0)
 
-    def update(self):
+    def sense_neighborhood(self, parent_cell):
+        pass
+
+    def update(self, parent_cell):
         """
         This method regulates the cell's temperature according to the temperature of its neighbors.
         """
