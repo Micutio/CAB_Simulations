c01fffc07bcac5021358c5035d8d8277790ab2bf
Michael Wagner <wagner.mchl@googlemail.com>
2018-11-27 00:06:46 +0800

    Reverted: Cell size and number back from testing values

--- SimTemplateSingleFile/game_of_life_hex.py
+++ SimTemplateSingleFile/game_of_life_hex.py
@@ -31,9 +31,9 @@ class GC(cab_gc.GlobalConstants):
         self.USE_HEX_CA = True
         self.USE_MOORE_NEIGHBORHOOD = True
         self.USE_CA_BORDERS = True
-        self.DIM_X = 3  # How many cells is the ca wide?
-        self.DIM_Y = 3  # How many cells is the ca high?
-        self.CELL_SIZE = 200  # How long/wide is one cell?
+        self.DIM_X = 75  # How many cells is the ca wide?
+        self.DIM_Y = 75  # How many cells is the ca high?
+        self.CELL_SIZE = 7  # How long/wide is one cell?
         self.GRID_WIDTH = self.DIM_X * self.CELL_SIZE
         self.GRID_HEIGHT = self.DIM_Y * self.CELL_SIZE
         ################################
