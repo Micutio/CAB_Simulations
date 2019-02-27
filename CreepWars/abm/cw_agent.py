4690b212f115f8212e89eeaedc3ffef82ed3ab89
Michael Wagner <wagner.mchl@googlemail.com>
2019-02-27 11:15:44 +0800

    Added: Stub for Hive agent implementation and completed creeplings

--- CreepWars/abm/cw_agent.py
+++ CreepWars/abm/cw_agent.py
@@ -3,25 +3,32 @@ Insert a module description
 """
 
 import random
+from operator import attrgetter
+
 from cab.abm.agent import CabAgent
 
 
 __author__ = 'Michael Wagner'
 __version__ = '1.0'
 
+class Team:
+    def __init__(self, number, color):
+        self.number = number
+        self.color = color
 
 class CreeplingAgent(CabAgent):
     """
-    Derive the new agent class from the CabAgent to let the CAB System know how to handle it.
+    Creeplings that walk around the landscape, spreading the creep of their team.
     """
     def __init__(self, x, y, gc, team, min_density, strategy_walk, strategy_seed, power):
         super().__init__(x, y, gc)
-        self.team = team
         self.min_density = min_density
         self.strategy_walk = strategy_walk  # 0 - random, 1 - min-based
         self.strategy_seed = strategy_seed  # 0 - random, 1 - min-based
         self.score = 0
         self.power = power
+        self.color = team.color   
+        self.team = team.number
         
     def perceive_and_act(self, abm, ca):
         """
@@ -31,45 +38,47 @@ class CreeplingAgent(CabAgent):
         :param abm: Instance of the agent based model.
         :param ca: Instance of the cellular automaton.
         """
-        neighborhood = ca.get_agent_neighborhood(self.x, self.y, 1)
+        neighborhood = ca.get_empty_agent_neighborhood(self.x, self.y, 1)
+        possible_cells = [c for c in list(neighborhood.values()) if self.team == c.team and c.temperature >= self.min_density]
         
-        if self.strategy_walk == 0:
-            random.shuffle(possible_cells)
-            cell = random.choice(possible_cells)
-        else:  # self.strategy_walk == 1:
-            min_val = min(possible_cells, key=attrgetter('temperature'))
-            c_list = [c for c in possible_cells if c.temperature == min_val.temperature]
-            random.shuffle(c_list)
-            cell = random.choice(c_list)
+        if possible_cells:
+            if self.strategy_walk == 0:
+                random.shuffle(possible_cells)
+                cell = random.choice(possible_cells)
+            else:  # self.strategy_walk == 1:
+                min_val = min(possible_cells, key=attrgetter('temperature'))
+                c_list = [c for c in possible_cells if c.temperature == min_val.temperature]
+                random.shuffle(c_list)
+                cell = random.choice(c_list)
+            # Move to new cell
+            self.x = cell.x
+            self.y = cell.y
+            cell.inc_temperature(self.team, self.power)
+            self.score += cell.inc_temperature(self.team, self.power)
+        else:
+            self.dead = True
 
+class CreepHive(CabAgent):
+    """
+    Stationary hive, spawning creeplings
+    """
+    def __init__(self, x, y, gc, team):
+        super().__init__(x, y, gc)
+        self.team = team
+        self.size = 10
 
-    # This is the minimum configuration for an agent. It does not need more than
-    # to implement the perceive_and_act method from CabAgent to perform actions
-    # in the simulation.
-    #
-    # There are additional methods designed for interacting with simulation components
-    # via user input, which can be redefined just like init and perceive and act.
-    
-    def on_lmb_click(self, abm, ca):
-        """
-        Executed when the mouse is pointed at the agent and left mouse button clicked.
-        """
+    def perceive_and_act(self, abm, ca):
+        neighborhood = ca.get_empty_agent_neighborhood(self.x, self.y, 1)
+        possible_cells = list(neighborhood.values())
+        for c in possible_cells:
+            if c.team != self.team.number:
+                self.dead = true
+                break;
+
+class MasterAgent(CabAgent):
+    """
+    Spawn hives and assign the cells around them to their teams.
+    """
+    def __init(self, None, None, gc):
         pass
-
-    def on_rmb_click(self, abm, ca):
-        """
-        Executed when the mouse is pointed at the agent and right mouse button clicked.
-        """
-        pass
-
-    def on_mouse_scroll_up(self, abm, ca):
-        """
-        Executed when the mouse is pointed at the agent and wheel scrolled up.
-        """
-        pass
-
-    def on_mouse_scroll_down(self, abm, ca):
-        """
-        Executed when the mouse is pointed at the agent and wheel scrolled down.
-        """
-        pass
+        # TODO: Re-implement 'Thermal Creep' scenario-specific mechanics from abm.
